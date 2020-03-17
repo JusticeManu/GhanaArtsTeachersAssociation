dcb73d1b01e1a37ce777b6b18cf8a25e78d13faf
JusticeManu <44110484+JusticeManu@users.noreply.github.com>
2020-03-17 02:08:46 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+// Animations init
+new WOW().init();
+$('#datepicker').datepicker();
+$('#datepicker').on('changeDate', function() {
+    $('#my_hidden_input').val(
+        $('#datepicker').datepicker('getFormattedDate')
+    );
+});
